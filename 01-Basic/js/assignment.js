a27f66e4bedf7da1fcdda1262ed07737bf5c2f48
Tochukwu Nwagu <chitonwagu@gmail.com>
2026-05-07 22:39:50 +0100

    Assigment for the day done

--- 01-Basic/js/assignment.js
+++ 01-Basic/js/assignment.js
@@ -60,7 +60,7 @@ if (population > avgPopulation) {
 }
 
 /**
- * Type Conversion and Coercion﻿
+ * Type Conversion and Coercion
 Predict the result of these 5 operations without executing them:
 
 console.log('9' - '5'); // -> 4?
@@ -70,15 +70,44 @@ console.log('123' < 57); // -> false?
 console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143?
 Execute the operations to check if you were right.
  */
-console.log('9' - '5'); // -> 4?
-console.log('19' - '13' + '17'); // -> 617?
-console.log('19' - '13' + 17); // -> 23?
-console.log('123' < 57); // -> false?
-console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143?
+// console.log('9' - '5'); // -> 4?
+// console.log('19' - '13' + '17'); // -> 617?
+// console.log('19' - '13' + 17); // -> 23?
+// console.log('123' < 57); // -> false?
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> 1143?
+
 /**
+ * Equality Operators: == vs. ===
+Declare a variable numNeighbours based on a prompt input like this:
+
+prompt('How many neighbour countries does your contry have?');
+If there is only 1 neighbour, log to the console 'Only 1 border!' (use loose equality == for now).
+
+Use an else-if block to log 'More than 1 border' in case numNeighbours is greater than 1.
+
+Use an else block to log 'No borders' (this block will be executed when numNeighbours is 0 or any other value).
+
+Test the code with different values of numNeighbours, including 1 and 0.
+
+Change == to ===, and test the code again, with the same values of numNeighbours. Notice what happens when there is exactly 1 border! Why is this happening?
+
+Finally, convert numNeighbours to a number, and watch what happens now when you input 1.
+
+Reflect on why we should use the === operator and type conversion in this situation.
+ */
+// const numNeighbours = Number(prompt('How many neighbour countries does your contry have?'));
+// if (numNeighbours === 1) {
+//   console.log("Only 1 border!")
+// } else if (numNeighbours > 1) {
+//   console.log('More than 1 border!')
+// } else {
+//   console.log('No borders!')
+// }
+/**
+ *
+
  * CHALLENGE #1
 Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
-
 Your task is to write some code to help them:
 
 Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
