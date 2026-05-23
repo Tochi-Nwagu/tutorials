ef699bfc7cfce4611f70602520004512ae66561c
Tochukwu Nwagu <chitonwagu@gmail.com>
2026-05-23 13:14:18 +0100

    Challenge.js added

--- 01-Basic/js/assignment.js
+++ 01-Basic/js/assignment.js
@@ -1,30 +1,30 @@
-let country = 'Nigeria';
-// let continent = 'Africa';
-let population = 241000000
+// let country = 'Nigeria';
+// // let continent = 'Africa';
+// let population = 241000000
 
 
-const isIsland = false;
-let language;
+// const isIsland = false;
+// let language;
 
 // console.log(country, population, isIsland, language);
 
 /**
- * let, const and var﻿
+ * let, const and var
 Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
 
 Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
 
 Try to change one of the changed variables now, and observe what happens.
  */
-language = "English";
-const continent = "Africa"
+// language = "English";
+// const continent = "Africa"
 
 //reassigning a const will show squiggly red line under the reassigned variable
 
 // console.log(country, continent, population, isIsland, language);
 
 /**
- * Basic Operators﻿
+ * Basic Operators
 If your country split in half, and each half would contain half the population, then how many people would live in each half?
 
 Increase the population of your country by 1 and log the result to the console.
@@ -35,29 +35,29 @@ The average population of a country is 33 million people. Does you country have 
 
 Based on the variables you created, create a new variable description which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese'.
  */
-let nupopulation = 241 / 2
-// console.log(nupopulation);
-nupopulation++;
-// console.log(nupopulation)
-let popFinland = 6000000
-const higherPopulation = population > popFinland
-// console.log(higherPopulation)
-const avgPopulation = 33000000
+// let nupopulation = 241 / 2
+// // console.log(nupopulation);
+// nupopulation++;
+// // console.log(nupopulation)
+// let popFinland = 6000000
+// const higherPopulation = population > popFinland
+// // console.log(higherPopulation)
+// const avgPopulation = 33000000
 
-const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`
+// const description = `${country} is in ${continent}, and its ${population} people speak ${language}.`
 // console.log(description)
 
 /**
- * Taking Decisions: if / else Statements﻿
+ * Taking Decisions: if / else Statements
 If your country's population is greater than 33 million, log a string like this to the console: "Portugal's population is 22 million below average" (the 22 is the average of 33 minus the country's population).
 
 After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
  */
-if (population > avgPopulation) {
-  console.log(`${country}'s population is ${population - avgPopulation} above average`)
-} else {
-  console.log(`${country}'s population is ${avgPopulation - population} below average`)
-}
+// if (population > avgPopulation) {
+//   console.log(`${country}'s population is ${population - avgPopulation} above average`)
+// } else {
+//   console.log(`${country}'s population is ${avgPopulation - population} below average`)
+// }
 
 /**
  * Type Conversion and Coercion
@@ -104,71 +104,36 @@ Reflect on why we should use the === operator and type conversion in this situat
 //   console.log('No borders!')
 // }
 /**
- *
+ * Logical Operators
+Comment out the previous code so the prompt doesn't get in the way.
 
- * CHALLENGE #1
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
-Your task is to write some code to help them:
+Let's say Sarah is looking for a new country to live in. She wants to live in a country that speaks English, has less than 50 million people and is not an island.
 
-Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+Write an if statement to help Sarah figure out if your country is right for her. You will need to write a condition that accounts for all of Sarah's criteria. Take your time with this, and check part of the solution if necessary.
 
-Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+If yours is the right country, log a strings like this 'You should live in Portugal :)'. If not, log 'Portugal does not meet your criteria :('.
 
-Log the value of BMIMark and BMIJohn to the console.
-
-BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
-
-TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
-
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
-
-
-
-👋 OPTIONAL: You can watch my solution in video format in the next lecture
-
-
-
-IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+Probably your country does not meet all the criteria. So go back and temporarily change some variables in order to make the condition true (unless you live in Canada :D).
  */
-// SOLUTION
-// const massMark = 78;
-// const heightMark = 1.69;
-// const massMark2 = 95;
-// const heightMark2 = 1.88;
-
-// const massJohn = 92;
-// const heightJohn = 1.95;
-// const massJohn2 = 85;
-// const heightJohn2 = 1.76;
-
-// const BMIMark = massMark / (heightMark * heightMark);
-// const BMIJohn = massJohn / (heightJohn * heightJohn);
-// const BMIMark2 = massMark2 / (heightMark2 * heightMark2);
-// const BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
-// const markHigherBMI = BMIMark > BMIJohn;
-
-// console.log(BMIMark, BMIJohn, BMIMark2, BMIJohn2);
-// console.log(markHigherBMI);
-
+// if (language === 'English' && population < 50000000 && !isIsland) {
+//   console.log('You should live in Nigeria :)')
+// } else {
+//   console.log('Nigeria does not meet your criteria :(')
+// }
 
 /**
- * Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+ * The switch Statement﻿
+Use a switch statement to log the following string for the given language:
 
-1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+chinese or mandarin: 'MOST number of native speakers!';
 
-"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+spanish: '2nd place in number of native speakers';
 
-2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+english: '3rd place';
 
-Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+hindi: 'Number 4';
 
-Note: Don't round the BMI values. Leave them as they are.
+arabic: '5th most spoken language';
 
-👋 OPTIONAL: You can watch my solution in video format in the next lecture
-
-IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+for all other simply log 'Great language too :D'.
  */
-
-// console.log("Mark" + "'s" + " BMI " + BMIMark + " is higher than John" + "'s!");
-// console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
-
---
+++ 01-Basic/js/challenge.js
@@ -0,0 +1,106 @@
+/**
+ *
+ * CHALLENGE #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+ */
+// SOLUTION
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massMark2 = 95;
+// const heightMark2 = 1.88;
+
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const massJohn2 = 85;
+// const heightJohn2 = 1.76;
+
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// const BMIMark2 = massMark2 / (heightMark2 * heightMark2);
+// const BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn, BMIMark2, BMIJohn2);
+// console.log(markHigherBMI);
+
+
+/**
+ * Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+
+"Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+Example: "Mark's BMI (28.3) is higher than John's (23.9)!" or "John's BMI (29.1) is higher than Mark's (27)!".
+
+Note: Don't round the BMI values. Leave them as they are.
+
+👋 OPTIONAL: You can watch my solution in video format in the next lecture
+
+IMPORTANT: The ** operator is not supported in this editor. Please make sure to use exactly this formula mass / (height * height), and not this one mass / (height ** 2).
+ */
+
+// console.log("Mark" + "'s" + " BMI " + BMIMark + " is higher than John" + "'s!");
+// console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`)
+
+/**
+ * CHALLENGE 3
+ * There are two gymnastics teams: Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+
+Your tasks:
+
+1. Calculate the average score for each team, using the test data included below. The average score for Dolphins should be assigned to the scoreDolphins variable, and the average score of Koalas should be assigned to the scoreKoalas variable.
+
+2. Compare the team's average scores to determine the winner of the competition, and print to the console:
+
+"Dolphins win the trophy" if Dolphins win, or
+
+"Koalas win the trophy" if Koalas win, or
+
+"Both win the trophy" if their average scores are equal.
+
+
+
+TEST DATA: Dolphins scored 96, 108, and 89. Koalas scored 88, 91, and 110.
+
+
+ */
+
+// let scoreDolphins = 96 + 108 + 89;
+// let scoreKoalas = 88 + 91 + 110
+
+// let avgDolphins = scoreDolphins / 3;
+// let avgKoalas = scoreKoalas / 3;
+
+// console.log(avgDolphins, avgKoalas)
+
+// if (avgDolphins > avgKoalas) {
+//   console.log('Dolphins win the trophy!!!');
+// } else if (avgDolphins === avgKoalas) {
+//   console.log('Both teams wins!!!');
+// } else {
+//   console.log('Koalas win the trophy!!!');
+// }
